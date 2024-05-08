be38e1d2dd0c3b271a983453918aa9d8f7defe8e
hyunmin200 <hwanghyunmin0650@gmail.com>
2024-05-08 22:31:26 +0900

    server side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,21 +1,16 @@
-"use client";
+export const metadata = {
+	title: "Home",
+};
 
-import { useEffect, useState } from "react";
-import Navigation from "../../components/navigation";
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Page() {
-	const [isLoading, setIsLoading] = useState(true);
-	const [Movies, setMovies] = useState([]);
-	const getMovies = async () => {
-		const response = await fetch(
-			"https://nomad-movies.nomadcoders.workers.dev/movies",
-		);
-		const json = await response.json();
-		setMovies(json);
-		setIsLoading(false);
-	};
-	useEffect(() => {
-		getMovies();
-	}, []);
-	return <div>{isLoading ? "Loading" : JSON.stringify(Movies)}</div>;
+async function getMovies() {
+	const response = await fetch(URL);
+	const json = await response.json();
+	return json;
+}
+
+export default async function HomePage() {
+	const movies = await getMovies();
+	return <div>{JSON.stringify(movies)}</div>;
 }
